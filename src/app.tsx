e37be650c8a4a36782ed3eb82b44ed166b4d7ff0
Todd <todd@Todds-MacBook-Air.local>
2025-06-18 09:34:17 -0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -7,9 +7,11 @@ export function App() {
   return (
     <div className='space-y-4 p-4'>
       <Config />
-      <FileInput
-        onChange={async (file) => setImage(await createImageBitmap(file))}
-      />
+      {!image && (
+        <FileInput
+          onChange={async (file) => setImage(await createImageBitmap(file))}
+        />
+      )}
       {image && (
         <div className='inline-flex gap-4'>
           <Palette image={image} />
